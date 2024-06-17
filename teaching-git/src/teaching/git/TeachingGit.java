0b0b94f69164988d4f8a73637cf6bf18da90dbf7
irvin <irvin@GLEZ-PC.lan>
2024-06-16 23:03:10 -0600

    First commit

---
+++ teaching-git/src/teaching/git/TeachingGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teaching.git;
+
+/**
+ *
+ * @author irvin
+ */
+public class TeachingGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This project is for teaching Git use!");
+    }
+    
+}
